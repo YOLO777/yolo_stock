b8c2018057a6302f7913899f508bb39d186ea50e
YOLO <1272220612@qq.com>
2021-03-15 09:56:49 +0800

    测试

--- yolo-stock/src/main/java/com/yolo/stock/StockBootstrap.java
+++ yolo-stock/src/main/java/com/yolo/stock/StockBootstrap.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * ${DESCRIPTION}
  *
- * @author wanghaobin
+ * @author
  * @create 2017-05-25 12:44
  */
 //@EnableDiscoveryClient
